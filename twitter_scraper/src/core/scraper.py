1b7202ecd36dd1f0bb31d6efd4d39ae5f4cdefb2
Thomas Byr <tbouyer2000@gmail.com>
2023-01-21 15:44:28 +0100

    `query` should be set once every api call

--- twitter_scraper/src/core/scraper.py
+++ twitter_scraper/src/core/scraper.py
@@ -6,31 +6,33 @@ import tweepy
 from dotenv import load_dotenv
 
 from ..classes.tweet import Tweet
-from .params import SearchParams
+from .query import SearchQuery
 
 __all__ = ['TwitterScraper']
 
 
 class TwitterScraper:
 
-  def __init__(self, params: SearchParams = None) -> None:
+  def __init__(self) -> None:
 
     load_dotenv()
     # api_key = os.getenv('API_KEY')
     # api_key_secret = os.getenv('API_KEY_SECRET')
     bearer_token = os.getenv('BEARER_TOKEN')
 
-    self._params = params
     self._client = tweepy.Client(bearer_token=bearer_token)
 
 
-  def search(self, query: str, limit: int = 100) -> list[Tweet]:
-    lang = self._params.lang if self._params else 'en'
-    start_date = self._params.start_date if self._params else None
-    end_date = self._params.end_date if self._params else None
+  def search(self, query: SearchQuery) -> list[Tweet]:
+    lang = query.lang if query.lang is not None else 'en'
+    start_date = query.start_date if query.start_date is not None else None
+    end_date = query.end_date if query.end_date is not None else None
+    limit = query.limit if query.limit is not None else 100
+
     dated_query = True
-    query = f'{query} lang:{lang}'
-    r: tweepy.Response
+    final_query = f'{query.subject} lang:{lang}'
+
+    r: tweepy.Response = None
     match start_date, end_date:
       case None, None:
         dated_query = False
@@ -43,10 +45,10 @@ class TwitterScraper:
 
     if dated_query:
       assert start_date is not None and end_date is not None
-      r = self._client.search_all_tweets(query=query, max_results=limit, start_time=start_date, end_time=end_date)
+      r = self._client.search_all_tweets(query=final_query, max_results=limit, start_time=start_date, end_time=end_date)
     else:
       assert start_date is None and end_date is None
-      r = self._client.search_recent_tweets(query=query, max_results=limit)
+      r = self._client.search_recent_tweets(query=final_query, max_results=limit)
 
     assert r is not None, 'No response from Twitter API'
     assert isinstance(r, tweepy.Response), 'Invalid response from Twitter API'
