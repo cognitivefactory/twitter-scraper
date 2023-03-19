55649582db4c23cd9e39ed7b0b7ac8c65026ab69
Thomas Byr <tbouyer2000@gmail.com>
2023-03-19 18:51:23 +0100

    bumped version

--- twitter_scraper/src/version.py
+++ twitter_scraper/src/version.py
@@ -34,9 +34,9 @@
 """Define Twitter-Scraper version information."""
 
 # We follow Semantic Versioning (https://semver.org/)
-_MAJOR_VERSION = '1'
-_MINOR_VERSION = '6'
-_PATCH_VERSION = '9'
+_MAJOR_VERSION = '2'
+_MINOR_VERSION = '0'
+_PATCH_VERSION = '1'
 
 # When building releases, we can update this value on the release branch to
 # reflect the current release candidate ('rc0', 'rc1') or, finally, the official
